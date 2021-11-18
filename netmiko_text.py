cf1537af11e709dcd9d90a5f9f1ff4f29b2a4bf5
BryanMcAdams <bryan.mcadams89@gmail.com>
2021-11-18 16:06:52 -0700

    Create netmiko_text.py

---
+++ netmiko_text.py
@@ -0,0 +1,18 @@
+from netmiko import ConnectHandler
+
+device1 = {
+    "host": "nxos1.lasthop.io",
+    "username": "pyclass",
+    "password": 88newclass,
+    "device_type": "cisco_nxos",
+}
+
+device2 = {
+    "host": "nxos2.lasthop.io",
+    "username": "pyclass",
+    "password": 88newclass,
+    "device_type": "cisco_nxos",
+}
+net_connect = ConnectHandler(device1)
+print(net_connect.find_prompt())
+net_connect.disconnect()
