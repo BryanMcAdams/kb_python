99ec43d0dca30f3dca844d3e9fa80e6224e8dd77
BryanMcAdams <bryan.mcadams89@gmail.com>
2021-11-18 16:08:12 -0700

    Update netmiko_test.py

--- netmiko_test.py
+++ netmiko_test.py
@@ -3,14 +3,14 @@ from netmiko import ConnectHandler
 device1 = {
     "host": "nxos1.lasthop.io",
     "username": "pyclass",
-    "password": 88newclass,
+    "password": '88newclass',
     "device_type": "cisco_nxos",
 }
 
 device2 = {
     "host": "nxos2.lasthop.io",
     "username": "pyclass",
-    "password": 88newclass,
+    "password": '88newclass',
     "device_type": "cisco_nxos",
 }
 net_connect = ConnectHandler(device1)
